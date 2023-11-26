12c6f1b96ae39bb7b18d131e29b5cb58824e24f1
Ifeanyi-Ani <ifeanyiani155@gmail.com>
2023-11-26 21:24:55 +0100

    working on top nav bar

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,3 +1,4 @@
+import "../globals.css";
 import Navbar from "@/components/shared/Navbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
@@ -5,12 +6,12 @@ import RightSidebar from "@/components/shared/RightSidebar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-dark-3">
         <Navbar />
-        <main>
+        <main className="main-container">
           <LeftSidebar />
           <section>
-            <div>{children}</div>
+            <div className="w-full max-w-4xl">{children}</div>
           </section>
           <RightSidebar />
         </main>
--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,7 +1,23 @@
-import React from "react";
-
+import { CiSearch } from "react-icons/ci";
+import { IoMdNotificationsOutline } from "react-icons/io";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar ">
+      <div>logo</div>
+      <div className="searchbar">
+        <CiSearch className="absolute top-[50%] left-[20px] translate-x-[-50%] translate-y-[-50%] text-[30px] my-auto " />
+        <input
+          type="search"
+          name="search"
+          id="search"
+          className="searchbar_input"
+        />
+      </div>
+      <div>
+        <IoMdNotificationsOutline />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
